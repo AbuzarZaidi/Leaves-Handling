0e89eca195bb7ad989055043571b3dbb3998bffd
AbuzarZaidi <abuzarzaidi947@gmail.com>
2022-10-02 15:03:13 +0500

    commit

--- server/app.js
+++ server/app.js
@@ -1,7 +1,19 @@
 const express = require('express')
-const app = express()
-const port = 5000
+const cors = require('cors');
+const bodyParser=require('body-parser')
 
+const usersRoutes=require('./routes/usersRoutes')
+const adminRoutes=require('./routes/adminRoutes')
+// require("./db/index.js");
+
+const app = express()
+
+
+app.use(cors());
+const port=process.env.PORT||5000;
+app.use(bodyParser.json())
+app.use('/user',usersRoutes)
+app.use('/admin',adminRoutes)
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
