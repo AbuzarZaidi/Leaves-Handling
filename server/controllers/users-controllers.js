e6087e36acb4714701126cf3412bf04d0888ffb1
AbuzarZaidi <abuzarzaidi947@gmail.com>
2022-10-02 17:02:51 +0500

    commit

--- server/controllers/users-controllers.js
+++ server/controllers/users-controllers.js
@@ -1,7 +1,12 @@
-const usersLeaves=require('../models/leaves')
+const UsersLeaves=require('../models/leaves')
 const leaveRequest=async(req,res,next)=>{
-    console.log('leaveRequest controller');
-console.log(req.body);
+    const  leave=new  UsersLeaves(req.body);
+    try {
+        await leave.save();
+        res.status(201).json(leave);
+    } catch (error) {
+        res.status(409).json({error:error.message})
+    }
 
   }
 
