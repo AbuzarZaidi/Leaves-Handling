626a940b46a4a2981095a5edd6d3dcc7800f9bcf
AbuzarZaidi <abuzarzaidi947@gmail.com>
2022-10-03 02:17:35 +0500

    commit

--- server/controllers/admin-controllers.js
+++ server/controllers/admin-controllers.js
@@ -1,19 +1,22 @@
+const User = require("../models/users");
 const updateLeavesRequest=async(req,res,next)=>{
-    // const  userId=req.params.uid;
-    // const updatedStatus=req.body.updatedStatus;
+    const  userId=req.params.uid;
+    const updatedStatus=req.body.updatedStatus;
        
-    // try {
-    //     const filter = { _id:userId };
-    //     if(updatedStatus==='accepted'||updatedStatus==='rejected'){
-    //         const update = { status: updatedStatus };
-    //         let result = await Leaves.findOneAndUpdate(filter, update);
-    //         res.status(201).json(result);
-    //     }else{
-    //         res.status(404).json("Something went wrong!")
-    //     }
-    // } catch (error) {
-    //     res.status(409).json({error:error.message})
-    // }
+    try {
+        const filter = { _id:userId };
+        if(updatedStatus==='accepted'||updatedStatus==='rejected'){
+            const user=await User.findById(userId)
+           user.leaveRequests.slice(-1)[0].status=updatedStatus 
+
+            let result = await User.findOneAndUpdate(filter, user);
+            res.status(201).json(result);
+        }else{
+            res.status(404).json("Something went wrong!")
+        }
+    } catch (error) {
+        res.status(409).json({error:error.message})
+    }
 }
 
 exports.updateLeavesRequest = updateLeavesRequest;
