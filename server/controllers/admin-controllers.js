4d0857169523ec0d09c0d505cc3bea3bbde36ae7
AbuzarZaidi <abuzarzaidi947@gmail.com>
2022-10-04 03:20:15 +0500

    commit

--- server/controllers/admin-controllers.js
+++ server/controllers/admin-controllers.js
@@ -1,41 +1,42 @@
 const User = require("../models/users");
 const HttpError = require("../models/http-error");
-const updateLeavesRequest=async(req,res,next)=>{
-    const  userId=req.params.uid;
-    const updatedStatus=req.body.updatedStatus;
-       if(updatedStatus==='accepted'||updatedStatus==='rejected'){
-        let user = "";
-        try {
-          user = await User.findById(userId);
-        } catch (err) {
-          const error = new HttpError("Could not find user for provided id.", 404);
-          return next(error);
-        }
-        user.leaveRequests.slice(-1)[0].status=updatedStatus 
-       user.save();
-       res.status(201).json("Status Updated!");
-       }
-       else{
-        return next(
-            new HttpError("Unknown Status", 404)
-          );
-       }
-    // try {
-    //     const filter = { _id:userId };
-    //     if(){
-    //         const user=await User.findById(userId)
-    //        user.leaveRequests.slice(-1)[0].status=updatedStatus 
+const updateLeavesRequest = async (req, res, next) => {
+  const userId = req.params.uid;
+  const updatedStatus = req.body.updatedStatus;
+  if (updatedStatus === "accepted" || updatedStatus === "rejected") {
+    let user = "";
+    try {
+      user = await User.findById(userId);
+    } catch (err) {
+      const error = new HttpError("Could not find user for provided id.", 404);
+      return next(error);
+    }
+    user.leaveRequests.slice(-1)[0].status = updatedStatus;
+    user.save();
+    res.status(201).json("Status Updated!");
+  } else {
+    return next(new HttpError("Unknown Status", 409));
+  }
+};
 
-    //         let result = await User.findOneAndUpdate(filter, user);
-    //         res.status(201).json(result);
-    //     }else{
-    //         res.status(404).json("Something went wrong!")
-    //     }
-    // } catch (error) {
-    //     res.status(409).json({error:error.message})
-    // }
-}
+const getApprovalRequest = async (req, res, next) => {
+  let result;
+  let updatedResult = [];
+  try {
+    result = await User.find({}, "leaveRequests");
+  } catch (err) {
+    const error = new HttpError("Something went wrong.", 500);
+    return next(error);
+  }
+  result.map((val) => {
+    if (val.leaveRequests.slice(-1)[0]) {
+      if (val.leaveRequests.slice(-1)[0].status == "pending") {
+        updatedResult.push(val);
+      }
+    }
+  });
+  res.status(200).json(updatedResult);
+};
 
 exports.updateLeavesRequest = updateLeavesRequest;
-
-
+exports.getApprovalRequest = getApprovalRequest;
--- server/routes/adminRoutes.js
+++ server/routes/adminRoutes.js
@@ -3,5 +3,6 @@ const express=require('express');
 const router=express.Router();
 
 router.post('/updateStatus/:uid',adminController.updateLeavesRequest);
+router.get('/getApprovalRequest',adminController.getApprovalRequest);
  
 module.exports = router;
