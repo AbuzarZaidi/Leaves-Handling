904235c7ed5d2a613e6c68fab322634c06ea7015
AbuzarZaidi <abuzarzaidi947@gmail.com>
2022-10-06 22:59:58 +0500

    commit

--- client/src/components/SideBar.jsx
+++ client/src/components/SideBar.jsx
@@ -1,11 +1,78 @@
-import React from 'react'
-import Box from '@mui/material/Box';
+import React from "react";
+import Box from "@mui/material/Box";
+import { Typography } from "@mui/material";
 const Sidebar = () => {
   return (
-    <Box >
+    <>
+      <Box sx={{ mt: 4, mx: "auto" }}>
+        <Box sx={{ ml: 5 }}>
+          <img src="/icons/logo.png" alt="" />
+        </Box>
 
-    </Box>
-  )
-}
+        <Box sx={{ display: "flex" }}>
+          <Typography
+            sx={{
+              fontFamily: "Serpentine",
+              fontWeight: 700,
+              fontSize: "25px",
+              color: "#00A1BB",
+              mr: 1,
+            }}
+          >
+            Mikro
+          </Typography>
+          <Typography
+            sx={{
+              fontFamily: "Serpentine",
+              fontWeight: 700,
+              fontSize: "25px",
+              color: "#00B7AD",
+              ml:0.5
+            }}
+          >
+            St
+          </Typography><img src="/icons/star.png" style={{marginTop:"10px"}}/>
+          <Typography
+            sx={{
+              fontFamily: "Serpentine",
+              fontWeight: 700,
+              fontSize: "25px",
+              color: "#00B7AD",
+              ml:"-10px"
+            }}
+          >
+            r
+          </Typography>
+        </Box>
 
-export default Sidebar
+        <Box sx={{ display: "flex" }}>
+          <Box sx={{ display: "flex", flexDirection: "column", mt: 0.5 }}>
+            <img src="/icons/logoLine.png" width="85px"></img>
+            <Typography
+              sx={{
+                fontFamily: "Serpentine-Light",
+                fontWeight: 400,
+                fontSize: "8px",
+                color: "#ffffff",
+              }}
+            >
+              You name it, We make it
+            </Typography>
+          </Box>
+          <Typography
+            sx={{
+              fontFamily: "Serpentine",
+              fontWeight: 700,
+              fontSize: "25px",
+              color: "#ffffff",
+            }}
+          >
+            Tech
+          </Typography>
+        </Box>
+      </Box>
+    </>
+  );
+};
+
+export default Sidebar;
