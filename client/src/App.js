533ba232df92bc011c9d06c9608bd777434d3701
AbuzarZaidi <abuzarzaidi947@gmail.com>
2022-10-07 13:11:24 +0500

    commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,28 @@
-import React from 'react'
-import {Grid} from './utlis/materialUIComponents'
-import SideBar from './components/SideBar'
-import Navbar from './components/Navbar'
-import ApplyForLeave from './pages/ApplyForLeave';
-// import EmployeesLeaves from './pages/EmployeesLeaves';
-// import MyLeaves from './pages/MyLeaves';
-// import LoginPage from './pages/LoginPage';
+import React from "react";
+import { Routes, Route} from "react-router-dom";
+
+import ApplyForLeave from "./pages/ApplyForLeave";
+import EmployeesLeaves from './pages/EmployeesLeaves';
+import MyLeaves from "./pages/MyLeaves";
+import LoginPage from "./pages/LoginPage";
+import PageStructure from "./components/PageStructure";
+
 
 function App() {
- 
+
+  
   return (
-    // <LoginPage/>
-      <Grid container >
-        <Grid item xs={0} md={2} lg={2} sx={{display:{xs:"none",md:"flex",lg:"flex"},backgroundColor:"#0A1833" ,height:"100vh",}}>
-          <SideBar/>
-        </Grid>
-        <Grid item xs={12} md={10} lg={10}>
-          <Navbar/>
-          {/* <MyLeaves/> */}
-          <ApplyForLeave/>
-          {/* <EmployeesLeaves/> */}
-          
-        </Grid>
-        </Grid>
-      
+    <>
+   
+
+          <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/myleaves" element={<PageStructure><MyLeaves /></PageStructure>} />
+          <Route path="/applyforleaves" element={<PageStructure><ApplyForLeave /></PageStructure>} />
+          <Route path="/employeesleaves" element={<PageStructure><EmployeesLeaves /></PageStructure>} />
+          </Routes>
     
+    </>
   );
 }
 
---
+++ client/src/components/PageStructure.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Grid } from "../utlis/materialUIComponents";
+import SideBar from './SideBar';
+import Navbar from './Navbar';
+const PageStructure = (props) => {
+  return (
+    <Grid container >
+    <Grid item xs={0} md={2} lg={2} sx={{display:{xs:"none",md:"flex",lg:"flex"},backgroundColor:"#0A1833" ,height:"100vh",}}>
+      <SideBar/>
+    </Grid>
+    <Grid item xs={12} md={10} lg={10}>
+      <Navbar/>
+      {props.children}  
+
+      </Grid>
+        </Grid>  
+  )
+}
+
+export default PageStructure
